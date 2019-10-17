fdf457419d9dac8628eca70fbb211cd491e42393
George <george_lee@brown.edu>
2019-10-16 20:41:04 -0400

    finished mockup

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ var menuData = [
 
 var gmap_style = {
     paddingTop: "200px"
-}
+};
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -142,82 +142,85 @@ function App() {
             {/* the contents */}
             <div className="flex-center-wrap">
                 {/* the images */}
-                <div className="images">
-                    <img src={require("./images/bigPhoto.png")}></img>
-                    <div className="flex-center-wrap">
-                        <img
-                            class="small-pic"
-                            src={require("./images/small1.png")}
-                        ></img>
-                        <img
-                            class="small-pic"
-                            src={require("./images/small2.png")}
-                        ></img>
-                        <img
-                            class="small-pic"
-                            src={require("./images/small3.png")}
-                        ></img>
-                        <img
-                            class="small-pic"
-                            src={require("./images/small4.png")}
-                        ></img>
-                        <img
-                            class="small-pic"
-                            src={require("./images/small5.png")}
-                        ></img>
-                        <img
-                            class="small-pic"
-                            src={require("./images/small6.png")}
-                        ></img>
+                <div className="images flex-center-wrap">
+                    <div>
+                        <img src={require("./images/bigPhoto.png")}></img>
+                        <div className="flex-center-wrap">
+                            <img
+                                class="small-pic"
+                                src={require("./images/small1.png")}
+                            ></img>
+                            <img
+                                class="small-pic"
+                                src={require("./images/small2.png")}
+                            ></img>
+                            <img
+                                class="small-pic"
+                                src={require("./images/small3.png")}
+                            ></img>
+                            <img
+                                class="small-pic"
+                                src={require("./images/small4.png")}
+                            ></img>
+                            <img
+                                class="small-pic"
+                                src={require("./images/small5.png")}
+                            ></img>
+                            <img
+                                class="small-pic"
+                                src={require("./images/small6.png")}
+                            ></img>
+                        </div>
+                        <div>Image 1 of 7</div>
                     </div>
-                    <div>Image 1 of 7</div>
+                </div>
+                <div className="text">
+                    <div>
+                        <div>
+                            <strong>Price:</strong> $125.00
+                            <br />
+                            <strong>Location:</strong> Provicence, RI <br />
+                            <strong>Posted:</strong> 2 Hours Ago by johnsmith34{" "}
+                            <br />
+                            <strong>Condition:</strong> Good <br />
+                            <strong>Price:</strong> $125.00 <br />
+                            <StarIcon />
+                            <StarIcon />
+                            <StarIcon />
+                            <StarIcon />
+                            <StarIcon />
+                            (45)
+                        </div>
+                        <p>
+                            For your consideration is a vintage Mid-Century
+                            seascape oil painting. This dramatic seascape was
+                            completed on a canvas panel and was signed by the
+                            artist on the lower left. The signature looks to me
+                            like C. Rodaphele, however, I am not positive. I
+                            provided a close-up image of the signature for you
+                            own inspection. The artist dramatically captures the
+                            rough seas crashing upon the rocky shore as a lone
+                            sailboat cruises beneath the storm clouds. I am
+                            estimating the painting to be from the 1940's-1950's
+                            time period. <br /> <br />
+                            This painting is in overall very good vintage
+                            condition. Under a black light it shows no signs of
+                            paint loss, in-painting or repair. Unframed the
+                            painting measures 14" in height x 18" wide and
+                            together with the rustic drift wood colored wooden
+                            frame it measures 18 1/2" x 22 1/2". The handsome
+                            vintage frame has a depth of 2" and is in overall
+                            very good condition consistent with its age.
+                        </p>
+                    </div>
                 </div>
                 {/* the text */}
-                <div className="flex-between-wrap column text">
-                    <div>
-                        <strong>Price:</strong> $125.00
-                        <br />
-                        <strong>Location:</strong> Provicence, RI <br />
-                        <strong>Posted:</strong> 2 Hours Ago by johnsmith34{" "}
-                        <br />
-                        <strong>Condition:</strong> Good <br />
-                        <strong>Price:</strong> $125.00 <br />
-                        <StarIcon />
-                        <StarIcon />
-                        <StarIcon />
-                        <StarIcon />
-                        <StarIcon />
-                        (45)
-                    </div>
-                    <p>
-                        For your consideration is a vintage Mid-Century seascape
-                        oil painting. This dramatic seascape was completed on a
-                        canvas panel and was signed by the artist on the lower
-                        left. The signature looks to me like C. Rodaphele,
-                        however, I am not positive. I provided a close-up image
-                        of the signature for you own inspection. The artist
-                        dramatically captures the rough seas crashing upon the
-                        rocky shore as a lone sailboat cruises beneath the storm
-                        clouds. I am estimating the painting to be from the
-                        1940's-1950's time period. <br /> <br />
-                        This painting is in overall very good vintage condition.
-                        Under a black light it shows no signs of paint loss,
-                        in-painting or repair. Unframed the painting measures
-                        14" in height x 18" wide and together with the rustic
-                        drift wood colored wooden frame it measures 18 1/2" x 22
-                        1/2". The handsome vintage frame has a depth of 2" and
-                        is in overall very good condition consistent with its
-                        age.
-                    </p>
-                </div>
-                <div style={gmap_style} className="wtf-is-happening">
-                    <div className="gmap flex-center-wrap">
-                        <img
-                            className="gmap-img "
-                            src={require("./images/maps.png")}
-                        ></img>
-                        <div className="circle "></div>
-                    </div>
+                <div className="gmap flex-center-wrap">
+                    <img
+                        className="gmap-img "
+                        src={require("./images/maps.png")}
+                    ></img>
+                    <div className="circle "></div>
                 </div>
             </div>
         </div>
